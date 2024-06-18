5752e88fe1793d141bbed3d5372cf6192927e060
Yeying-Xingchen <yeyingxingchen@yeah.net>
2024-06-18 18:23:33 +0800

    修改 开启重写工作。

--- lightningrobot/plugin.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-from . import log
-class Plugin(ABC):
-    @abstractmethod
-    async def on_command(self, message: str,id) -> None:
-        """处理接收到的消息"""
-        await log.info("收到来自"+id+"的消息：",message)
-        #未来在此处添加判断用户。
-        pass
